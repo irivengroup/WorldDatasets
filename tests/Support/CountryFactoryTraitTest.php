a5ef519482b49fca6ed4824dd2357d120640a5ec
Alfred TCHONDJO <altimagni@yahoo.fr>
2026-04-13 11:02:28 +0200

    update : refactor testMakeCountriesReturnsExpectedFixtureSet

--- tests/Support/CountryFactoryTraitTest.php
+++ tests/Support/CountryFactoryTraitTest.php
@@ -25,12 +25,17 @@ final class CountryFactoryTraitTest extends TestCase
 
     public function testMakeCountriesReturnsExpectedFixtureSet(): void
     {
-        /** @var iterable<CountryInfo> $countries */
         $countries = $this->makeCountries();
-        $countriesList = iterator_to_array($countries, false);
 
-        self::assertCount(4, $countriesList);
-        self::assertContainsOnlyInstancesOf(CountryInfo::class, $countriesList);
-        self::assertSame(['FR', 'DE', 'US', 'JP'], array_map(static fn (CountryInfo $country): string => $country->alpha2(), $countriesList));
+        self::assertCount(4, $countries);
+
+        $codes = [];
+        foreach ($countries as $country) {
+            self::assertInstanceOf(CountryInfo::class, $country);
+            $codes[] = $country->alpha2();
+        }
+
+        self::assertSame(['FR', 'DE', 'US', 'JP'], $codes);
     }
 }
+
